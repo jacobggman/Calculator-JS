a6939e911caece87af5c9007e0f2d7ab159ef4ac
jacobggman <hgecdkto@gmail.com>
2024-12-02 23:49:20 +0200

    Catching errors

--- source.js
+++ source.js
@@ -5,24 +5,57 @@
 // make worker for culc
 
 import calculate from './calculate.js';
+import {TokenParseException} from './tokenize.js';
+import {CulcParseException, DivByZeroException} from './calculate.js';
+
 
 let display;
+let valueAsStr = "";
 
 window.onload = function() {
     display = document.getElementById('display');
 };
 
 function appendValue(value) {
-  display.value += value;
+  valueAsStr += value;
   display.scrollLeft = display.scrollWidth;
+  updateView();
 }
 
 function clearDisplay() {
-  display.value = '';
+  valueAsStr = "";
+  updateView()
 }
 
 function calculateInput() {
-    display.value = calculate(display.value);
+  if (!valueAsStr)
+  {
+    updateView(); // For if have error
+    return;
+  }
+
+  try {
+      valueAsStr = calculate(display.value).toString();
+      updateView();
+    } catch (error) {
+      valueAsStr = "";
+      if (error instanceof DivByZeroException)
+      {
+        display.value = "Div by zero error";
+      }
+      else if (error instanceof CulcParseException || error instanceof TokenParseException )
+      {
+        display.value = "Format error";
+      }
+      else
+      {
+        throw error;
+      }
+    }
+}
+
+function updateView() {
+  display.value = valueAsStr;
 }
 
 window.calculateInput = calculateInput;
